c11114e939b2a768931b023b7a912b4e288a631d
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-05-09 14:52:27 +0200

    update

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@ import {connect} from 'mongoose'
 connect('mongodb://127.0.0.1:27017/shahad')
 
 
+
 //Använder variable app för att arbeta med express
 const app = express()
 // const port  = 8000
